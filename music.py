b85a109f51c3d2a6a0955963ccddf2859bfdeb0a
Cathy97 <liyuyingcathy@163.com>
2018-12-02 13:08:41 -0500

    GET top N songs in a cluster

--- music.py
+++
@@ -1,6 +0,0 @@
-from PythonSrc.hdf5_getters import *
-
-h5 = open_h5_file_read("../project/MillionSongSubset/data/A/A/A/TRAAAFD128F92F423A.h5")
-duration = hdf5_getters.get_duration(h5)
-# get_artist_id("../project/MillionSongSubset/data/A/A/A/+TRAAAFD128F92F423A.h5",songidx=0)
-h5.close()
